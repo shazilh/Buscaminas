c7011cde716c541c7c530b34f7a681104b10b7c0
Shazil Tovar <shazil@gmail.com>
2017-04-04 16:51:46 -0500

    cambiado con mas eventos

--- main.js
+++ main.js
@@ -1,58 +1,77 @@
-  //Asignar valores a los cuadros de todas las filas
-  var cuadro1 = document.getElementById("0,0");
-  var cuadro2 = document.getElementById("1,0");
-  var cuadro3 = document.getElementById("2,0");
-  var cuadro4 = document.getElementById("3,0");
-  var cuadro5 = document.getElementById("0,1");
-  var cuadro6 = document.getElementById("1,1");
-  var cuadro7 = document.getElementById("2,1");
-  var cuadro8 = document.getElementById("3,1");
-  var cuadro9 = document.getElementById("0,2");
-  var cuadro10 = document.getElementById("1,2");
-  var cuadro11 = document.getElementById("2,2");
-  var cuadro12 = document.getElementById("3,2");
-  var cuadro13 = document.getElementById("0,3");
-  var cuadro14 = document.getElementById("1,3");
-  var cuadro15 = document.getElementById("2,3");
-  var cuadro16 = document.getElementById("3,3");
+var longitud=document.getElementsByClassName("espacio").length;
+var longitudBombas=document.getElementsByClassName("bomba").length;
+var longitudNumero1=document.getElementsByClassName("numero1").length;
+var longitudNumero2=document.getElementsByClassName("numero2").length;
+var tabla=document.getElementById("tablaBuscaminas");
 
-  cuadro1.addEventListener("click",mostrar);
-  cuadro2.addEventListener("click",mostrar);
-  cuadro3.addEventListener("click",mostrar);
-  cuadro4.addEventListener("click",cambiarColor);
-  cuadro5.addEventListener("click",mostrar);
-  cuadro6.addEventListener("click",mostrar);
-  cuadro7.addEventListener("click",mostrar);
-  cuadro8.addEventListener("click",mostrar);
-  cuadro9.addEventListener("click",cambiarColor);
-  cuadro10.addEventListener("click",mostrar);
-  cuadro11.addEventListener("click",mostrar);
-  cuadro12.addEventListener("click",mostrar);
-  cuadro13.addEventListener("click",cambiarColor);
-  cuadro14.addEventListener("click",mostrar);
-  cuadro15.addEventListener("click",mostrar);
-  cuadro16.addEventListener("click",cambiarColor);
+//En la variable longitud se guardan el numero de clases "espacio" que tengo en mi documento
+var numEspacios =[];
+var celdasBomba=[];
+var celdasNumero1=[];
+var celdasNumero2=[];
 
-  function mostrar(){
-    cuadro1.innerText="1";
-    cuadro2.innerText="Buum!";
-    cuadro3.innerText="1";
-
-    cuadro5.innerText="1";
-    cuadro6.innerText="2";
-    cuadro7.innerText="2";
-    cuadro8.innerText="1";
-
-    cuadro10.innerText="1";
-    cuadro11.innerText="Buum!";
-    cuadro12.innerText="2";
-
-    cuadro14.innerText="2";
-    cuadro15.innerText="1";
-    cuadro16.innerText=" ";
-  }
-function cambiarColor(){
-  cuadro4.style.background="green";
-  cuadro9.style.background="green";
-  cuadro13.style.background="green";
+//Este primer for obtiene los espacios y los mete en el arreglo numEspacios
+for (var i = 0; i < longitud; i++){
+  numEspacios[i]=document.getElementsByClassName("espacio")[i];
 }
+for (var i=0; i< numEspacios.length; i++){
+  numEspacios[i].addEventListener("click",cambiarColor,false);
+}
+//Este primer for obtiene las casillas con bomba y los mete en el arreglo celdasBomba
+for (var i = 0; i < longitudBombas; i++){
+  celdasBomba[i]=document.getElementsByClassName("bomba")[i];
+}
+for (var i=0; i< celdasBomba.length; i++){
+  celdasBomba[i].addEventListener("click",explotaBomba,false);
+}
+//Este primer for obtiene las casillas con numero1 y los mete en el arreglo celdasnumeros1
+for (var i = 0; i < longitudNumero1; i++){
+  celdasNumero1[i]=document.getElementsByClassName("numero1")[i];
+}
+for (var i=0; i< celdasNumero1.length; i++){
+  celdasNumero1[i].addEventListener("click",muestraNum1,false);
+}
+//Este primer for obtiene las casillas con numero2 y los mete en el arreglo celdasNumero2
+for (var i = 0; i < longitudNumero2; i++){
+  celdasNumero2[i]=document.getElementsByClassName("numero2")[i];
+}
+for (var i=0; i< celdasNumero2.length; i++){
+  celdasNumero2[i].addEventListener("click",muestraNum2,false);
+}
+//Se definen aquí las funciones.
+/*function stop(){
+  event.stoppropagation;
+  alert("No puedes jugar más!, Da clic en reiniciar para jugar otra vez!")
+}*/
+function cambiarColor(){
+this.style.backgroundColor = "green";
+}
+function explotaBomba(){
+  alert("Encontraste una Bomba,PERDISTE!!!");
+  this.innerHTML="<img src='bomba.jpg' width='70'>";
+  if(explotaBomba==true){
+    event.stopPropagation();
+  }
+
+  //desabilitar;
+  //event.stopPropagation();
+}
+function muestraNum1(){
+  this.innerText="1";
+}
+function muestraNum2(){
+  this.innerText="2";
+}
+//Esta funcion resetea todo el form que contiene la tablaBuscaminas
+function reiniciar(){
+  tabla.reset();
+}
+/*
+function desabilitarTabla(){
+ tabla.disabled = true;
+}
+
+function habilitarTabla() {
+ tabla.disabled= false;
+}
+*/
